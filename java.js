09f624cc340b8e98e6f43a3f463d98c81dd78bba
Umairnaeemkhan <155988400+Umairnaeemkhan@users.noreply.github.com>
2024-01-25 16:54:39 +0500

    Add files via upload

---
+++ java.js
@@ -0,0 +1,9 @@
+// Assignment1
+
+var age = "18 year old"
+var isStudent = " Yes i'm becholors student"
+console.log(age);
+console.log(isStudent);
+alert("Welcome to java script class. My name is  Umair khattak and i'm full stack developer")
+// Assignment1
+
